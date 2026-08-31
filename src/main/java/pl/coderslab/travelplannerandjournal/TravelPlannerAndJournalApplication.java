d5f816f89d11871437fd123cd73262436a75880a
klau44 <ksawa@visa.com>
2026-08-31 16:33:22 +0200

    init commit

---
+++ src/main/java/pl/coderslab/travelplannerandjournal/TravelPlannerAndJournalApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.travelplannerandjournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelPlannerAndJournalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelPlannerAndJournalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/coderslab/travelplannerandjournal/TravelPlannerAndJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.travelplannerandjournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelPlannerAndJournalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
