1271aef9c29c807d7cdf72e4bb27debbe7ec76bf
klau44 <ksawa@visa.com>
2026-09-08 16:56:42 +0200

    Geoapify configuration

---
+++ src/main/java/pl/coderslab/travelplannerandjournal/configuration/GeoapifyConfig.java
@@ -0,0 +1,17 @@
+package pl.coderslab.travelplannerandjournal.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class GeoapifyConfig {
+
+    @Bean
+    public RestClient restClient(@Value("${geoapify.base-url}") String baseUrl) {
+        return RestClient.builder()
+                .baseUrl(baseUrl)
+                .build();
+    }
+}
