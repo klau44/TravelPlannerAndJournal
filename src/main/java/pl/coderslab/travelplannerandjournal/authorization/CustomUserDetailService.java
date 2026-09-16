f47d6e76a602c8e1000412533b1a0de41e9700e0
klau44 <ksawa@visa.com>
2026-09-16 15:13:17 +0200

    Change exception message

--- src/main/java/pl/coderslab/travelplannerandjournal/authorization/CustomUserDetailService.java
+++ src/main/java/pl/coderslab/travelplannerandjournal/authorization/CustomUserDetailService.java
@@ -17,7 +17,7 @@ public class CustomUserDetailService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("Nie znaleziono użytkownika"));
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
 
         return new CustomUserDetails(user);
     }
