afb6c8c9dda07a12eff4420e1e3a49eeb811400d
klau44 <ksawa@visa.com>
2026-09-16 15:32:01 +0200

    Change user retrieval for trip management

--- src/main/java/pl/coderslab/travelplannerandjournal/controller/TripController.java
+++ src/main/java/pl/coderslab/travelplannerandjournal/controller/TripController.java
@@ -3,8 +3,9 @@ package pl.coderslab.travelplannerandjournal.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import pl.coderslab.travelplannerandjournal.authorization.CustomUserDetails;
 import pl.coderslab.travelplannerandjournal.model.TripRequest;
 import pl.coderslab.travelplannerandjournal.model.TripResponse;
 import pl.coderslab.travelplannerandjournal.service.TripService;
@@ -19,28 +20,33 @@ public class TripController {
     private final TripService tripService;
 
     @GetMapping
-    public ResponseEntity<List<TripResponse>> findAll(Authentication authentication) {
-        return new ResponseEntity<>(tripService.findAll(authentication), HttpStatus.OK);
+    public ResponseEntity<List<TripResponse>> findAll(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        Long userId = userDetails.getUser().getId();
+        return new ResponseEntity<>(tripService.findAll(userId), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<TripResponse> findById(@PathVariable Long id, Authentication authentication) {
-        return new ResponseEntity<>(tripService.findById(id, authentication), HttpStatus.OK);
+    public ResponseEntity<TripResponse> findById(@PathVariable Long id, @AuthenticationPrincipal CustomUserDetails userDetails) {
+        Long userId = userDetails.getUser().getId();
+        return new ResponseEntity<>(tripService.findById(id, userId), HttpStatus.OK);
     }
 
     @PostMapping
-    public ResponseEntity<TripResponse> add(@RequestBody TripRequest tripRequest, Authentication authentication) {
-        return new ResponseEntity<>(tripService.add(tripRequest, authentication), HttpStatus.CREATED);
+    public ResponseEntity<TripResponse> add(@RequestBody TripRequest tripRequest, @AuthenticationPrincipal CustomUserDetails userDetails) {
+        Long userId = userDetails.getUser().getId();
+        return new ResponseEntity<>(tripService.add(tripRequest, userId), HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<TripResponse> update(@PathVariable Long id, @RequestBody TripRequest tripRequest, Authentication authentication) {
-        return new ResponseEntity<>(tripService.update(id, tripRequest, authentication), HttpStatus.CREATED);
+    public ResponseEntity<TripResponse> update(@PathVariable Long id, @RequestBody TripRequest tripRequest, @AuthenticationPrincipal CustomUserDetails userDetails) {
+        Long userId = userDetails.getUser().getId();
+        return new ResponseEntity<>(tripService.update(id, tripRequest, userId), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id, Authentication authentication) {
-        tripService.delete(id, authentication);
+    public ResponseEntity<Void> delete(@PathVariable Long id, @AuthenticationPrincipal CustomUserDetails userDetails) {
+        Long userId = userDetails.getUser().getId();
+        tripService.delete(id, userId);
         return ResponseEntity.noContent().build();
     }
 }
